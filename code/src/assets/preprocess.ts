405ce374bfac1d26dbf297715e0b486c28bce381
bASILgIL <basil.gil@polymtl.ca>
2025-04-01 15:11:46 -0400

    todos preprocess

---
+++ code/src/assets/preprocess.ts
@@ -0,0 +1,5 @@
+// TODO : jai besoin dune fonction pour filtrer selon les ann/es genre d<ann/e x \ ann/e y
+// TODO : jai besoin d'une fonction qui enleve les gardiens et enleve les colonnes associées avec gardiens
+// TODO : Pour l’attribut games_played, nous avons remplacé les valeurs NULL par 0 pour les joueurs n’ayant jamais évolué dans la LNH.
+// TODO : Pour l’attribut goalie_games_played, nous avons appliqué le même traitement en remplaçant les valeurs NULL par 0 pour les gardiens n’ayant jamais joué dans la LNH.
+// TODO : Pour l’attribut position, nous avons standardisé les valeurs afin d’assurer une meilleure cohérence des données. Les modifications suivantes ont été appliquées : «C; LW» → «C/LW», «C RW» → «C/RW», «Centr» → «C», «C/R» → «C/RW», «L/RW»→ «LW/RW».
