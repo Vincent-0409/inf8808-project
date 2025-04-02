e41087457e33586d926588e1b915e333d74490f6
Minh Ngoc Do <minhngocdo.mnd@gmail.com>
2025-04-02 12:46:20 -0400

    Update small-multiples component

--- code/src/app/components/small-multiples/small-multiples.component.ts
+++ code/src/app/components/small-multiples/small-multiples.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { DataPreprocessingService, DraftPlayer } from '../../services/preprocessing/preprocessing.service';
 
 @Component({
   selector: 'app-small-multiples',
-  standalone: true,
-  imports: [],
   templateUrl: './small-multiples.component.html',
-  styleUrl: './small-multiples.component.scss'
+  styleUrls: ['./small-multiples.component.scss']
 })
 export class SmallMultiplesComponent {
+  players: DraftPlayer[] = [];
+  groupedPlayers: { [year: number]: DraftPlayer[] } = {};
 
+  constructor(private dataService: DataPreprocessingService) {}
+
+  ngOnInit(): void {
+    this.dataService.loadDraftData().subscribe(data => {
+      console.log(data)
+      this.players = data;
+      this.groupPlayersByYear();
+    });
+  }
+
+  getYears(): string[] {
+    return Object.keys(this.groupedPlayers);
+  }
+  
+
+  groupPlayersByYear(): void {
+    this.groupedPlayers = this.players.reduce((acc, player) => {
+      if (!acc[player.year]) {
+        acc[player.year] = [];
+      }
+      acc[player.year].push(player);
+      return acc;
+    }, {} as { [year: number]: DraftPlayer[] });
+  }
 }
