34ded0e1a679b0f158d7b6354a181e0d18935023
Vincent Bellemare <vincent.bellemare@polymtl.ca>
2025-04-03 22:19:00 -0400

    Ajout de Histogram dans l'App

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HistogramComponent } from './histogram/histogram.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistogramComponent
   ],
   imports: [
     BrowserModule,
