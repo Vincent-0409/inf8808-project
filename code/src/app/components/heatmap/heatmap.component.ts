b1bee4285abe60aea278f4752fd22a2da2cad325
bASILgIL <basil.gil@polymtl.ca>
2025-04-14 19:41:53 -0400

    legende interactive

--- code/src/app/components/heatmap/heatmap.component.ts
+++ code/src/app/components/heatmap/heatmap.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 import * as d3Chromatic from 'd3-scale-chromatic';
 import { DataPreprocessingService, DraftPlayer } from '../../services/preprocessing/preprocessing.service';
+import { sliderRight } from 'd3-simple-slider';
 
 type HeatmapData = { year: number; stat: string; correlation: number; nb_players_considered: number };
 
@@ -12,11 +13,10 @@ type HeatmapData = { year: number; stat: string; correlation: number; nb_players
 })
 export class HeatmapComponent implements OnInit {
   private data: HeatmapData[] = [];
-  private svg: any;
   private margin = { top: 35, right: 200, bottom: 80, left: 200 };
   private xScale: d3.ScaleBand<string> = d3.scaleBand<string>().padding(0.05);
   private yScale: d3.ScaleBand<string> = d3.scaleBand<string>().padding(0.01);
-  private colorScale = d3.scaleSequential(d3Chromatic.interpolatePiYG);
+  private colorScale: any = d3.scaleSequential(d3Chromatic.interpolatePiYG);
 
   private bounds: any;
   private graphSize: any;
@@ -24,6 +24,13 @@ export class HeatmapComponent implements OnInit {
     width: number,
     height: number
   } = { width: window.innerWidth * 0.9, height: 650 };
+
+  private sliderScale: d3.ScaleLinear<number, number> = d3.scaleLinear();
+  private minValue: number = -1;
+  private maxValue: number = 1;
+
+  private grayOverlayTop: any;
+  private grayOverlayBottom: any;
 
   constructor(private dataService: DataPreprocessingService) {}
 
@@ -82,7 +89,14 @@ export class HeatmapComponent implements OnInit {
     .attr('y', d => this.yScale(d.stat)!)
     .attr('width', this.xScale.bandwidth())
     .attr('height', this.yScale.bandwidth())
-    .attr('fill', d => this.colorScale(d.correlation));
+    .attr('fill', d => {
+      if (d.correlation >= this.minValue && d.correlation <= this.maxValue) {
+        return this.colorScale(d.correlation);
+      } else {
+        const originalColor = d3.hsl(this.colorScale(d.correlation));
+        return d3.hsl(0, 0, originalColor.l).toString();
+      }
+    });
   }
 
   private setSizing () {
@@ -138,7 +152,21 @@ export class HeatmapComponent implements OnInit {
 
     this.setRectHandler();
 
-      // legend.draw(margin.left / 2, margin.top + 5, graphSize.height - 10, 15, 'url(#gradient)', colorScale)
+    this.initGradient()
+    this.initLegendBar()
+    this.initLegendAxis()
+
+    const legendX = this.margin.left + this.graphSize.width + 50;
+
+    this.drawLegend(
+      legendX,
+      this.margin.top,
+      this.graphSize.height,
+      15,
+      'url(#gradient)'
+    );
+
+    this.initSlider(legendX+25, this.margin.top-this.graphSize.height, this.graphSize.height);
   }
 
   private updateXScale() {
@@ -238,10 +266,6 @@ export class HeatmapComponent implements OnInit {
       .attr('font-weight', 'normal');
   }
 
-
-
-
-
   private initGradient() {
     const svg = d3.select('.heatmap-svg');
   
@@ -254,24 +278,23 @@ export class HeatmapComponent implements OnInit {
       .attr('y1', 1)
       .attr('x2', 0)
       .attr('y2', 0);
-  
-    // Get the domain of the color scale
-    const domain = this.colorScale.domain();
-  
-    // Use the domain's start and end values to create the gradient
-    const startColor = this.colorScale(domain[0]);
-    const endColor = this.colorScale(domain[1]);
-  
-    // Create a simple gradient with two stops (start and end)
+
+    type GradientStop = {
+      offset: string;
+      color: string;
+    };
+    
+    const stops: GradientStop[] = this.colorScale.ticks().map((tick: any, i: number, nodes: string | any[]) => ({
+      offset: `${100 * (i / nodes.length)}%`,
+      color: this.colorScale(tick)
+    }));
+    
     linearGradient
-      .append('stop')
-      .attr('offset', '0%')
-      .attr('stop-color', startColor);
-  
-    linearGradient
-      .append('stop')
-      .attr('offset', '100%')
-      .attr('stop-color', endColor);
+      .selectAll<SVGStopElement, GradientStop>('stop')
+      .data(stops)
+      .join('stop')
+      .attr('offset', d => d.offset)
+      .attr('stop-color', d => d.color);
   }
 
   private initLegendBar() {
@@ -286,33 +309,88 @@ export class HeatmapComponent implements OnInit {
       .attr('class', 'legend axis');
   }
 
-  // private drawLegend(
-  //   x: number,
-  //   y: number,
-  //   height: number,
-  //   width: number,
-  //   fill: string,
-  //   colorScale: d3.ScaleSequential<number, string>
-  // ) {
-  //   const svg = d3.select('.heatmap-svg');
+  private drawLegend(
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    fill: string
+  ) {
+    const svg = d3.select('.heatmap-svg');
   
-  //   svg.select('.legend.bar')
-  //     .attr('x', x)
-  //     .attr('y', y)
-  //     .attr('width', width)
-  //     .attr('height', height)
-  //     .attr('fill', fill);
+    svg.select('.legend.bar')
+      .attr('x', x)
+      .attr('y', y)
+      .attr('width', width)
+      .attr('height', height)
+      .attr('fill', fill);
   
-  //   const legendScale = d3.scaleLinear()
-  //     .domain(colorScale.domain())
-  //     .range([height, 0]);
+    const legendScale = d3.scaleLinear()
+      .domain(this.colorScale.domain())
+      .range([height, 0]);
   
-  //   const legendAxis = d3.axisBottom(legendScale)
-  //     .ticks(5);
-  
-  //   svg.select('.legend.axis')
-  //     .attr('transform', `translate(${x}, ${y + height + 10})`)  // Positioned beneath the heatmap
-  //     .call(legendAxis);
-  // }
-  
+    const legendAxis = d3.axisLeft(legendScale)
+      .ticks(5);
+
+    (svg.select('.legend.axis') as d3.Selection<SVGGElement, unknown, HTMLElement, undefined>)
+      .attr('transform', `translate(${x - width + 10}, ${y})`)
+      .call(legendAxis);
+    
+    this.grayOverlayTop = svg.append('rect')
+      .attr('class', 'legend-overlay top')
+      .attr('x', x)
+      .attr('y', y)
+      .attr('width', width)
+      .attr('height', height)
+      .attr('fill', 'none');
+
+    this.grayOverlayBottom = svg.append('rect')
+      .attr('class', 'legend-overlay bottom')
+      .attr('x', x)
+      .attr('y', y)
+      .attr('height', height)
+      .attr('width', width)
+      .attr('fill', 'none');
+  }
+
+  private initSlider(x: number, y: number, height: number) {
+    const svg = d3.select('.heatmap-svg');
+
+    this.sliderScale = d3.scaleLinear()
+      .domain(this.colorScale.domain())
+      .range([height, 0]);
+    
+    const slider = (sliderRight(this.sliderScale) as any)
+      .min(1)
+      .max(-1)
+      .height(height)
+      .default([-1, 1])
+      .step(0.01)
+      .ticks(0)
+      .fill('none')
+      .on('onchange', (val: [number, number]) => {
+        this.minValue = val[0];
+        this.maxValue = val[1];
+        this.updateRects();
+        this.updateSliderOverlays(height);
+      });
+
+    svg.append('g')
+    .attr('transform', `translate(${x}, ${y})`)
+    .call(slider);
+  }
+
+  private updateSliderOverlays(height: number) {
+    const minY = this.sliderScale(this.minValue);
+    const maxY = this.sliderScale(this.maxValue);
+
+    this.grayOverlayTop
+      .attr('height', maxY)
+      .attr('fill', 'rgba(117, 117, 117, 0.7)');
+
+    this.grayOverlayBottom
+      .attr('y', this.margin.top + minY)
+      .attr('height', height - minY)
+      .attr('fill', 'rgba(117, 117, 117, 0.7)');
+  }
 }
